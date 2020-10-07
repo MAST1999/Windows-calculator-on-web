04357b3adbad76822f480d7004fdf89f556b3461
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-10-08 00:29:52 +0330

    fully functional except for history and memory

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,48 @@
 let numbers = [];
 let actions = [];
 let changedNumbers = true; //This is for determining whether user has intentionally pressed zero and wants to use it
-let negative = false;
-const calculate = Event => {
-
+let done = false;
+const calculate = _ => {
+    if (changedNumbers) {
+        document.getElementById("holder").textContent += " " + document.getElementById("numbers-results").textContent;
+        numbers.push(parseFloat(document.getElementById("numbers-results").textContent));
+        document.getElementById("numbers-results").textContent = "0";
+        changedNumbers = false;
+    }
+    if (numbers.length < 2) return;
+    while (numbers.length > 1) {
+        console.log(actions[0], numbers[0], numbers[1]);
+        switch (actions[0]) {
+            case "+":
+                numbers[0] = numbers[0] + numbers[1];
+                numbers.splice(1, 1); break;
+            case "-":
+                numbers[0] = numbers[0] - numbers[1];
+                numbers.splice(1, 1); break;
+            case "×":
+                numbers[0] = numbers[0] * numbers[1];
+                numbers.splice(1, 1); break;
+            case "÷":
+                numbers[0] = numbers[0] / numbers[1];
+                numbers.splice(1, 1); break;
+            default: document.getElementById("numbers-results").textContent = "Please stop trying to be funny";
+                break;
+        }
+        actions.splice(0, 1);
+    }
+    document.getElementById("numbers-results").textContent = numbers[0];
+    done = true;
+    changedNumbers = true;
+    document.getElementById("holder").textContent += " =";
 };
 const addNumber = Event => {
+    if (done) {
+        document.getElementById("numbers-results").textContent = "0";
+        document.getElementById("holder").textContent = "";
+        numbers = [];
+        actions = [];
+        done = false;
+    }
     const btnNumber = Event.target;
     if (document.getElementById("numbers-results").textContent == "0") {
         document.getElementById("numbers-results").textContent = btnNumber.textContent;
@@ -26,21 +63,24 @@ const remover = _ => {
 const addFunctionShowAble = Event => {
     let functionShow = Event.target.textContent;
     if (changedNumbers) {
-        if (negative) {
-            
-            numbers.push(parseFloat(document.getElementById("numbers-results").textContent));
-        } else {
-            numbers.push(+document.getElementById("numbers-results").textContent);
-        }
+        numbers.push(parseFloat(document.getElementById("numbers-results").textContent));
         actions.push(functionShow);
-        document.getElementById("holder").textContent += " " + document.getElementById("numbers-results").textContent + " " + functionShow;
-        console.log(numbers);
+        console.log(numbers, actions);
         changedNumbers = false;
+        if (done === false) {
+            document.getElementById("holder").textContent += " " + document.getElementById("numbers-results").textContent + " " + functionShow;
+        } else {
+            document.getElementById("holder").textContent = document.getElementById("numbers-results").textContent + " " + functionShow;
+            numbers.splice(1, 1);
+            done = false;
+        }
         document.getElementById("numbers-results").textContent = "0";
     } else {
         actions[actions.length - 1] = functionShow;
         document.getElementById("holder").textContent = document.getElementById("holder").textContent.slice(0, document.getElementById("holder").textContent.length - 1);
         document.getElementById("holder").textContent += functionShow;
+        console.log(numbers, actions);
+        done = false;
     }
 };
 const resetCE = _ => {
@@ -55,12 +95,85 @@ const resetC = _ => {
 const togglePositivity = _ => {
     if (document.getElementById("numbers-results").textContent !== "0" && document.getElementById("numbers-results").textContent[0] !== "-") {
         document.getElementById("numbers-results").textContent = "-" + document.getElementById("numbers-results").textContent;
-        negative = true;
     } else if (document.getElementById("numbers-results").textContent !== "0") {
         document.getElementById("numbers-results").textContent = document.getElementById("numbers-results").textContent.slice(1);
-        negative = false;
     }
 };
+const oneDividedByNumber = _ => {
+    document.getElementById("numbers-results").textContent = 1 / parseFloat(document.getElementById("numbers-results").textContent);
+
+};
+const numberSquared = _ => {
+    document.getElementById("numbers-results").textContent = parseFloat(document.getElementById("numbers-results").textContent) * parseFloat(document.getElementById("numbers-results").textContent);
+    document.getElementById("holder").textContent += " " + "sqr(" + document.getElementById("numbers-results").textContent + ")";
+};
+const numberCubed = _ => {
+    document.getElementById("holder").textContent += " " + document.getElementById("numbers-results").textContent + " ^ 3";
+    document.getElementById("numbers-results").textContent = parseFloat(document.getElementById("numbers-results").textContent) * parseFloat(document.getElementById("numbers-results").textContent) * parseFloat(document.getElementById("numbers-results").textContent);
+};
+const root = _ => {
+    document.getElementById("holder").textContent += " " + "root(" + document.getElementById("numbers-results").textContent + ")";
+    document.getElementById("numbers-results").textContent = Math.sqrt(parseFloat(document.getElementById("numbers-results").textContent));
+};
+const decimal = _ => {
+    if (document.getElementById("numbers-results").textContent.indexOf(".") === -1) {
+        document.getElementById("numbers-results").textContent += ".";
+    }
+};
+const percent = _ => {
+    if (actions.length < 1) {
+        resetC();
+        document.getElementById("holder").textContent = "0";
+        return;
+    }
+    switch (actions[actions.length - 1]) {
+        case "+":
+            if (numbers.length === actions.length) {
+                numbers.push((numbers[numbers.length - 1] / 100) * parseFloat(document.getElementById("numbers-results").textContent));
+            } else {
+                return;
+            }
+            document.getElementById("holder").textContent += " " + numbers[numbers.length - 1];
+            changedNumbers = false; break;
+        case "-":
+            if (numbers.length === actions.length) {
+                numbers.push((numbers[numbers.length - 1] / 100) * parseFloat(document.getElementById("numbers-results").textContent));
+            } else {
+                return;
+            }
+            document.getElementById("holder").textContent += " " + numbers[numbers.length - 1];
+            changedNumbers = false; break;
+        case "×":
+            if (numbers.length === actions.length) {
+                numbers.push((parseFloat(document.getElementById("numbers-results").textContent / 100)));
+            } else {
+                let temp = numbers[numbers.length - 1].toString();
+                temp = temp.length;
+                document.getElementById("holder").textContent = document.getElementById("holder").textContent.slice(0, document.getElementById("holder").textContent.length - 1 - temp);
+                numbers[numbers.length - 1] = parseFloat(document.getElementById("numbers-results").textContent / 100);
+            }
+            document.getElementById("holder").textContent += " " + numbers[numbers.length - 1];
+            document.getElementById("numbers-results").textContent = numbers[numbers.length - 1];
+            changedNumbers = false; console.log(numbers, actions);
+            break;
+        case "÷":
+            if (numbers.length === actions.length) {
+                numbers.push((parseFloat(document.getElementById("numbers-results").textContent / 100)));
+            } else {
+                let temp = numbers[numbers.length - 1].toString();
+                temp = temp.length;
+                document.getElementById("holder").textContent = document.getElementById("holder").textContent.slice(0, document.getElementById("holder").textContent.length - 1 - temp);
+                numbers[numbers.length - 1] = parseFloat(document.getElementById("numbers-results").textContent / 100);
+            }
+            document.getElementById("holder").textContent += " " + numbers[numbers.length - 1];
+            document.getElementById("numbers-results").textContent = numbers[numbers.length - 1];
+            changedNumbers = false; console.log(numbers, actions);
+            break;
+        default: document.getElementById("numbers-results").textContent = "Please stop trying to be funny";
+            break;
+    }
+};
+
 (function addingEventListeners() {
     document.getElementsByClassName("equals")[0].addEventListener("click", calculate, false);
     const btnNumbers = document.querySelectorAll(".numbers");
@@ -75,4 +188,10 @@ const togglePositivity = _ => {
     document.getElementsByClassName("ce")[0].addEventListener("click", resetCE, false);
     document.getElementsByClassName("c")[0].addEventListener("click", resetC, false);
     document.getElementsByClassName("plus-minus")[0].addEventListener("click", togglePositivity, false);
+    document.getElementsByClassName("one-divided-x")[0].addEventListener("click", oneDividedByNumber, false);
+    document.getElementsByClassName("x-squared")[0].addEventListener("click", numberSquared, false);
+    document.getElementsByClassName("cube-x")[0].addEventListener("click", numberCubed, false);
+    document.getElementsByClassName("dot")[0].addEventListener("click", decimal, false);
+    document.getElementsByClassName("squared-x")[0].addEventListener("click", root, false);
+    document.getElementsByClassName("percent")[0].addEventListener("click", percent, false);
 }());
