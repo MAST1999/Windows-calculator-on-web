9cdb9bab1f97f2a702f18b2362abc91f2db6ba5a
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2021-05-19 10:10:21 +0430

    server is done

--- server/server.js
+++ server/server.js
@@ -1,13 +1,40 @@
-const express = require("express");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const app = express();
+const server = http.createServer((req, res) => {
+  console.log("request", req.url);
 
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 4001;
+  let filePath = "." + req.url;
+  if (filePath === "./") {
+    filePath = "./index.html";
+  }
 
-app.use(express.static("../"));
-app.get("/", (req, res) => {
-  res.send("../index.html");
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let mimeTypes = {
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".json": "text/json",
+  };
+
+  let contentType = mimeTypes[extname] || "application/octet-stream";
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code) {
+        fs.readFile("./404.html", (err, data) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(data, "utf-8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end("sorry, check with website admin for the error: " + err.code);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data, "utf-8");
+    }
+  });
 });
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(3000);
+console.log("server is running at http://127.0.0.1:3000");
